fcbe9cecdefa70efbefbf1e07e248c75a4f9ef98
ldrohan <drohanluke@gmail.com>
2014-06-22 17:21:05 -0700

    info window showing description

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,78 +1,83 @@
-
 function initialize() {
-  var mapOptions = {
-    zoom: 11,
-    streetViewControl: false
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    var mapOptions = {
+        zoom: 11,
+        streetViewControl: false
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-  //HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);    
-      var image = 'http://www.topfurnitures.com/wp-content/themes/TheJewelryShopDark/images/shopping_icon.jpg';
-      var current = 'http://img.lib.msu.edu/mobile/user_icon_g.png';
-      
-        
-        
-        
-      var marker = new google.maps.Marker({
-        position: pos,
-        draggable:false,
-        animation: google.maps.Animation.DROP,
-        icon: current,
-        map: map,
-        title:"Current Location"
-        });
+    //HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+            var image = 'http://www.topfurnitures.com/wp-content/themes/TheJewelryShopDark/images/shopping_icon.jpg';
+            var current = 'http://img.lib.msu.edu/mobile/user_icon_g.png';
 
-      $.ajax('/items.json', {type: 'get'}).success(function(data){
-          for (var x in data) {
-            console.log(data[x]);
-            console.log(data[x]["lat"]);
-            
-        
-               var items = new google.maps.Marker({
-                position: new google.maps.LatLng(data[x]["lat"],data[x]["long"]),
-                draggable:false,
+
+
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                draggable: false,
                 animation: google.maps.Animation.DROP,
-                icon: image,
+                icon: current,
                 map: map,
-                title:data[x]["description"]
-               });  
+                title: "Current Location"
+            });
 
-          
-         }
+            $.ajax('/items.json', {
+                type: 'get'
+            }).success(function(data) {
+                for (var x in data) {
+                    console.log(data[x]);
+                    console.log(data[x]["lat"]);
+                    var infowindow = new google.maps.InfoWindow();
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(data[x]["lat"], data[x]["long"]),
+                        draggable: false,
+                        animation: google.maps.Animation.DROP,
+                        icon: image,
+                        content: data[x]["description"],
+                        map: map,
+                        title: data[x]["description"]
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.setContent(this.title);
+                        infowindow.open(map, this);
+                    });
+
+                }
+            });
+
+
+
+            map.setCenter(pos);
+        }, function() {
+            handleNoGeolocation(true);
         });
-
-
-
-      map.setCenter(pos);
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
 }
 
 function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
-  }
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
 
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(60, 105),
-    content: content
-  };
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: content
+    };
 
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
