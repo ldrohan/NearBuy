f9dd92b03fb7eb74c9783c6041fbbf7e649f7f37
ldrohan <drohanluke@gmail.com>
2014-06-27 16:42:06 -0700

    updating scraping url

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -153,16 +153,19 @@ function initialize() {
                                 var savePhone = this.phoneInfo
                                 var saveLat = this.latInfo
                                 var saveLong = this.longInfo
-                                if(savePhone != null){
-                                infowindow.setContent('<h3 id="infowindowtitle" style="text-align:center;">' + saveName + '</h3>' + '</br>' +
+	                                if(allData === '/favorite.json'){
+	                                	$('#remove').append('<button id="removefavorite">Remove Shit</button>');
+	                                }
+                                if(savePhone != null){                                	
+                                	infowindow.setContent('<h3 id="infowindowtitle" style="text-align:center;">' + saveName + '</h3>' + '</br>' +
                                     '<IMG id="thumbnail" SRC=' + saveImage + '>' + '<span id="infowindowbody">' + '</br>' +
                                     saveDescription + '</br>' + '</br>' + 'Email Address: ' + saveEmail + '</br>' +
-                                    'Phone: ' + '<span id="phonespan">' + savePhone + '<span>' + '</br>' + '<span>' + '<span id ="sentemail">' + '</br>' + '<form id="emailform"><div>Subject: Hi! I\'m Interested In Your Post...<div><input id="from" placeholder="Your Email Address"></input></br><textarea id="body" placeholder="Say Something Nice..."></textarea></br><button class="button" id="submit" type="submit">Send Email!</button></form>' + '</span>' + '<button class="button" id="favorite" type="submit">Add To Favorites!</button>'
+                                    'Phone: ' + '<span id="phonespan">' + savePhone + '<span>' + '</br>' + '<span>' + '<span id ="sentemail">' + '</br>' + '<form id="emailform"><div>Subject: Hi! I\'m Interested In Your Post...<div><input id="from" placeholder="Your Email Address"></input></br><textarea id="body" placeholder="Say Something Nice..."></textarea></br><button class="button" id="submit" type="submit">Send Email!</button></form>' + '</span>' + '<button class="button" id="favorite" type="submit">Add To Favorites!</button><div id="remove">hi</div>'
                                 );
                               }else{
                               	infowindow.setContent('<h3 id="infowindowtitle" style="text-align:center;">' + saveName + '</h3>' + '</br>' +
                                     '<IMG id="thumbnail" SRC=' + saveImage + '>' + '<span id="infowindowbody">' + '</br>' +
-                                    saveDescription + '</br>' + '</br>' + 'Email Address: ' + saveEmail + '</br>' + '</br>' + '<span>' + '<span id ="sentemail">' + '<form id="emailform"><div>Subject: Hi! I\'m Interested In Your Post...<div><input id="from" placeholder="Your Email Address"></input></br><textarea id="body" placeholder="Say Something Nice..."></textarea></br><button class="button" id="submit" type="submit">Send Email!</button></form>' + '</span>' + '<button class="button" id="favorite" type="submit">Add To Favorites!</button>'
+                                    saveDescription + '</br>' + '</br>' + 'Email Address: ' + saveEmail + '</br>' + '</br>' + '<span>' + '<span id ="sentemail">' + '<form id="emailform"><div>Subject: Hi! I\'m Interested In Your Post...<div><input id="from" placeholder="Your Email Address"></input></br><textarea id="body" placeholder="Say Something Nice..."></textarea></br><button class="button" id="submit" type="submit">Send Email!</button></form>' + '</span>' + '<button class="button" id="favorite" type="submit">Add To Favorites!</button>' + '<div id="remove"></div>'
                                 );
 
                               }
